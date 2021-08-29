3f64b21bad41b9b735ec3907f151554affbf7221
André Almeida <andalvalmeida@gmail.com>
2021-08-29 18:51:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Node.js Template
+
+This is a template for creating a new project with Node.js.
+
+This template is configured to work with [TypeScript](https://www.typescriptlang.org/) (4.4.2) and [Express](https://expressjs.com/pt-br/) (4.17.1) framework.
+
+To help with code padronization it's already configured with [EditorConfig](https://editorconfig.org/), [ESLint](https://eslint.org/) (using Airbnb style guide) and [Prettier](https://prettier.io/).
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
+import { AppError } from './errors/AppError';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.code).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: `Internal Server Error - ${err.message}`,
+    });
+});
+
+app.listen(3333, () => console.log('Server is running!!!'));
